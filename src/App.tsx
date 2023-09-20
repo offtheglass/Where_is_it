1d64d592f9f35cb0f54c68c9c1481c7ec226c7c7
offtheglass <86214286+offtheglass@users.noreply.github.com>
2023-09-20 15:12:22 +0900

    first_commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState,useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MapNaverDefault from './MapNaverDefault'
+function App() {
+  
+  return (
+    <MapNaverDefault/>
+  );
+}
+
+export default App
---
+++ src/Service.tsx
@@ -0,0 +1,16 @@
+import { Container as MapDiv, NaverMap, Marker } from 'react-naver-maps'
+
+
+function Service() {
+
+
+  return (
+     <div>
+      <button onClick={(event)=>{console.log(event)}}>
+        서비스선택
+      </button>
+    </div>
+  )
+}
+
+export default Service
